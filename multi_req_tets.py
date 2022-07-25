0bc9b30891931e2ff8a34416aa3ecec203742882
SangheonYi <tmzpdpdlwl@gmail.com>
2022-07-25 09:46:56 +0900

    feat: Feat server, client monitor

--- multi_req_tets.py
+++ multi_req_tets.py
@@ -6,35 +6,47 @@ host = 'http://127.0.0.1:5000'
 short_text = "오늘 11kg 간다. 우리가 강의를 이상헌 듣는다. 농협은 일요일 쉰다."
 paragraph_count = 100
 user_count = 100
-req_count = 100
-body = {
-        "batches":
-        [
-            {
-                "path": "./xyz/Downloads",
-                "text": [short_text for _ in range(paragraph_count)],
-            },
-        ],
-        "labels": [],
-        "options": {
-            "word": "False"
-        }
-    }
+req_count = 20
+
 one_req_spent_avg = 0
 total_spent = 0
 responsed_count = 0
+th_cnt = 0
+total_req_cnt = 0
 
 def req():
     global total_spent
     global responsed_count
     global one_req_spent_avg
+    global th_cnt
+    global total_req_cnt
+
+    th_cnt += 1
+    client_id = th_cnt
     for i in range(req_count):
         start = time.time()
+        print(f"😀{client_id}'s {i}th sent")
+        body = {
+            "batches":
+            [
+                {
+                    "path": "./xyz/Downloads",
+                    "text": [short_text for _ in range(paragraph_count)],
+                },
+            ],
+            "labels": [],
+            "options": {
+                "word": "False"
+            },
+            "client_id":client_id, 
+            "req_cnt":i
+        }
         response = requests.post(f'{host}/pii_demo',json=body).json()
         total_spent += time.time() - start
         responsed_count += 1
         one_req_spent_avg = round(total_spent / responsed_count, 3)
-        print(f"took {one_req_spent_avg} from {response['process']}")
+        print(f"{client_id}'s {i}th took {one_req_spent_avg} from {response['process']}")
+        total_req_cnt += 1
     
 threads = []
 e2e_start = time.time()
@@ -46,4 +58,4 @@ for i in range(user_count):
 for t in threads:
     t.join()
 print(f'paragraph_count: {paragraph_count} user_count: {user_count} req_count: {req_count}')
-print(f'total req sum: {round(total_spent, 3)} avg: {one_req_spent_avg}, e2e took: {round(time.time() - e2e_start, 3)}')
+print(f'total req {total_req_cnt} spent sum: {round(total_spent, 3)} avg: {one_req_spent_avg}, e2e took: {round(time.time() - e2e_start, 3)}')
--- plot.py
+++ plot.py
@@ -2,21 +2,49 @@ import matplotlib.pyplot as pl
 import statistics
 import torch
 
-def draw_log(graph, target):
-      with open(target + '_log.txt', 'r', encoding='utf-8') as file:
-        splited = file.readline().split(', ')
-        data = [float(e) for e in splited if e]
-        x_size = len(data)
-        margin = x_size // 400
-        graph.set_xlim(-margin, x_size + margin)
-        graph.plot(range(x_size), data[:x_size])
-        graph.set_title(f"{torch.__version__} {target} log {round(statistics.mean(data), 3)}")
+def draw_graph(graph, data, target):
+  x_size = len(data)
+  margin = x_size // 400
+  graph.set_xlim(-margin, x_size + margin)
+  graph.plot(range(x_size), data[:x_size])
+  graph.set_title(f"{target} log {round(statistics.mean(data), 3)}")
+  
+def parse_serv_log():
+  with open('../ai-masking-server/logs/ai-masking-daemon.log', 'r', encoding='utf-8') as file:
+    data = []
+    for line in file:
+      splited = line.split()
+      if 'spent' in splited:
+        data.append(float(splited[-1]))
+  return data
 
+def parse_cli_log():
+  with open('time_log.txt', 'r', encoding='utf-8') as file:
+    data = []
+    for line in file.readlines():
+      if '😀'in line:
+        continue
+      splited = line.split()
+      if "from" in splited:
+        data.append(float(splited[3]))
+  return data
+
+def draw_log(target):
+  with open(target + '_log.txt', 'r', encoding='utf-8') as file:
+    splited = file.readline().split(', ')
+    data = [float(e) for e in splited if e]
+  return data
 
 axis = pl.subplots(2, 1, figsize=(20, 10))[1]
 pl.subplots_adjust(left=0.03, bottom=0.05, right=0.98, top=0.98, wspace=1, hspace=0.1)
 
-draw_log(axis[0], 'memory')
+# data = draw_log('memory')
+# draw_graph(axis[0], data, 'memory')
+# data = draw_log('time')
+# draw_graph(axis[1], data, 'time')
+data = parse_serv_log()
+draw_graph(axis[0], data, 'server')
+data = parse_cli_log()
+draw_graph(axis[1], data, 'client')
 
-draw_log(axis[1], 'time')
 pl.savefig('test.png')
